e7b3e7a621ce8db8e93831655bb615aa816f2831
hardikshah1991 <143990064+hardikshah1991@users.noreply.github.com>
2023-09-04 11:53:53 +0530

    Synchronizing job Test-job from AWS Glue at 2023/09/04 06:23:53

---
+++ Test-job/Test-job.py
@@ -0,0 +1,20 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+# Your "Hello, World!" message
+message = "Hello, World!"
+
+# Print the message
+print(message)
+
+job.commit()
